8c22dc4ed498a562b1c836295cb01cf2dd57fe20
antoniominoru <antonio.minoru.ops@gmail.com>
2023-08-07 22:09:52 -0300

    create extracts_contents and your dependencies

--- source/build_cub/build_data.c
+++ source/build_cub/build_data.c
@@ -1,6 +1,18 @@
 
 #include "cub3d.h"
 
+
+static void extract_contents(t_data *data, char **file)
+{
+	data->map->no_path = extract_content(file, "NO ",1);
+	data->map->so_path = extract_content(file, "SO ",1);
+	data->map->we_path = extract_content(file, "WE ",1);
+	data->map->ea_path = extract_content(file, "EA ",1);
+	data->map->str_sky = extract_content(file, "C ",1);
+	data->map->str_floor = extract_content(file, "F ",1);
+	extract_map(data, file);
+}
+
 int	build_cub(t_data *data)
 {
 	char	**file;
---
+++ source/build_cub/extract_maps.c
@@ -0,0 +1,72 @@
+#include "cub3d.h"
+
+static char	*complet_line(int columns, char *line)
+{
+	int		index;
+	int		size_line;
+	char	*result;
+
+	index = 0;
+	size_line = ft_strlen(line);
+	result = NULL;
+	result = ft_calloc(sizeof(char), columns + 1);
+	while (index < columns)
+	{
+		if (index < size_line)
+			result[index] = line[index];
+		else
+			result[index] = ' ';
+		index++;
+	}
+	result[index] = '\0';
+	return (result);
+}
+
+static void	map_size(t_data *data, char **file)
+{
+	int		index;
+	int		lines;
+	size_t	columns;
+
+	index = 0;
+	lines = 0;
+	columns = 0;
+	while (file[index] != NULL)
+	{
+		if (is_line_map(file[index]))
+		{
+			if (columns < ft_strlen(file[index]))
+				columns = ft_strlen(file[index]);
+			lines++;
+		}
+		index++;
+	}
+	data->map->nb_columns = columns;
+	data->map->nb_lines = lines;
+	data->map->height_px = lines * TILE_SIZE;
+	data->map->width_px = columns * TILE_SIZE;
+}
+
+void	extract_map(t_data *data, char **file)
+{
+	int		index;
+	char	*temp;
+	char	**result;
+
+	index = 0;
+	result = NULL;
+	temp = NULL;
+	map_size(data, file);
+	while (file[index] != NULL)
+	{
+		if (is_line_map(file[index]))
+		{
+			temp = complet_line(data->map->nb_columns, file[index]);
+			result = ft_matrix_join(result, temp);
+			ft_strdel(&temp);
+			temp = NULL;
+		}
+		index++;
+	}
+	data->map->map_matrix = result;
+}
