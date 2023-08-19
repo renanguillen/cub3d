8b9a114493107749e7d06fb2de75452289396628
antoniominoru <antonio.minoru.ops@gmail.com>
2023-08-18 21:43:09 -0300

    create render_3d

--- source/raycasting/raycasting.c
+++ source/raycasting/raycasting.c
@@ -1,5 +1,49 @@
 #include "cub3d.h"
 
+double	dist_between_points(double x1, double y1, double x2, double y2)
+{
+	double	ret;
+
+	ret = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+	return (ret);
+}
+
+static void	calc_dist(t_data *data, t_ray *ray)
+{
+	if (ray->hrz->found_wall)
+		ray->hrz->distance = dist_between_points(data->player->pos_x,
+				data->player->pos_y, ray->hrz->wall_x, ray->hrz->wall_y);
+	else
+		ray->hrz->distance = MAX_INT;
+	if (ray->vert->found_wall)
+		ray->vert->distance = dist_between_points(data->player->pos_x,
+				data->player->pos_y, ray->vert->wall_x, ray->vert->wall_y);
+	else
+		ray->vert->distance = MAX_INT;
+	if (ray->vert->distance < ray->hrz->distance)
+	{
+		ray->distance = ray->vert->distance
+			* cos(normalize_angle(data->player->angle - ray->ray_angle));
+		ray->vertical_wall = 1;
+	}
+	else
+	{
+		ray->distance = ray->hrz->distance
+			* cos(normalize_angle(data->player->angle - ray->ray_angle));
+		ray->vertical_wall = 0;
+	}
+}
+
+void	clear_inter(t_inter *inter)
+{
+	inter->found_wall = 0;
+	inter->wall_x = 0;
+	inter->wall_y = 0;
+	inter->step_x = 0;
+	inter->step_y = 0;
+	inter->distance = 0;
+}
+
 void	clear_ray(t_ray *ray)
 {
 	clear_inter(ray->hrz);
---
+++ source/raycasting/render_3d.c
@@ -0,0 +1,57 @@
+#include "cub3d.h"
+
+static void	draw_sky(t_data *data, t_ray *ray, int x)
+{
+	int	y;
+
+	y = 0;
+	while (y < ray->render->wall_top_pixel)
+	{
+		draw_pixel_color(data->image, x, y, data->map->color_sky);
+		y++;
+	}
+}
+
+static void	draw_bottom(t_data *data, t_ray *ray, int x)
+{
+	int	y;
+
+	y = ray->render->wall_bottom_pixel;
+	while (y < WIN_HEIGHT)
+	{
+		draw_pixel_color(data->image, x, y, data->map->color_floor);
+		y++;
+	}
+}
+
+static void	calculate_columns(t_ray *ray)
+{
+	ray->render->wall_top_pixel = (WIN_HEIGHT / 2)
+		- (ray->render->proj_wall_height / 2);
+	if (ray->render->wall_top_pixel < 0)
+		ray->render->wall_top_pixel = 0;
+	ray->render->wall_bottom_pixel = (WIN_HEIGHT / 2)
+		+ (ray->render->proj_wall_height / 2);
+	if (ray->render->wall_bottom_pixel > WIN_HEIGHT)
+		ray->render->wall_bottom_pixel = WIN_HEIGHT;
+}
+
+void	render_3d_projected_walls(t_data *data)
+{
+	int		x;
+	t_ray	*ray;
+
+	x = 0;
+	while (x < NUM_RAYS)
+	{
+		ray = &data->rays[x];
+		ray->render->proj_wall_height = (TILE_SIZE / ray->distance)
+			* data->dist_proj_plane;
+		ray->render->wall_strip_height = ceil(ray->render->proj_wall_height);
+		calculate_columns(ray);
+		draw_sky(data, ray, x);
+		draw_wall(data, ray, x);
+		draw_bottom(data, ray, x);
+		x++;
+	}
+}
